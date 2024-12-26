bc1988304e863b00bca2a576ac3b7e2fd1bac7b3
stephanoumenos <stefano@dechecchi.dev>
2024-12-27 00:21:55 +0100

    Working with vLLM

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	openai "github.com/openai/openai-go"
+	option "github.com/openai/openai-go/option"
+	"github.com/openai/openai-go/shared"
+)
 
 func main() {
-	fmt.Println("Hello world")
+	client := openai.NewClient(
+		option.WithBaseURL("http://localhost:8000/v1/chat"),
+	)
+
+	/*
+		models, err := client.Models.List(context.Background())
+		if err != nil {
+			fmt.Printf("Error listing models: %v\n", err)
+			return
+		}
+		model := models.Data[0].ID
+	*/
+
+	chatCompletion, err := client.Completions.New(context.TODO(), openai.CompletionNewParams{
+		Prompt:      openai.F[openai.CompletionNewParamsPromptUnion](shared.UnionString("You are the best vegan activist that has ever existed.")),
+		Model:       openai.F(openai.CompletionNewParamsModel("Qwen/Qwen2.5-7B-Instruct")),
+		MaxTokens:   openai.F(int64(512)),
+		Temperature: openai.F(1.000000),
+	})
+	if err != nil {
+		fmt.Printf("Error creating completion: %v\n", err)
+		return
+	}
+
+	fmt.Println(chatCompletion.Choices[0].Text)
 }
