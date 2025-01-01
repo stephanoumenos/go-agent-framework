5eabdb86556c4da4a55fa4bc2d668e49b725ab16
stephanoumenos <stefano@dechecchi.dev>
2025-01-01 01:21:32 +0100

    add supervisor (WIP)

--- supervisor/supervisor.go
+++ supervisor/supervisor.go
@@ -1,39 +1,68 @@
 package supervisor
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
-type contextKey struct{}
+type dryRunContextKey struct{}
 
-var ctxKey = &contextKey{}
+var dryRunCtxKey = &dryRunContextKey{}
 
-func Supervise(ctx context.Context, run func(context.Context) ImplicitGraph) {
-	dryRun(ctx, run)
+type dryRunNodes struct{}
+
+type dryRunContextValue struct {
+	nodes []*dryRunNodes
 }
 
+func newDryRunContextValue() *dryRunContextValue {
+	return &dryRunContextValue{}
+}
+
+func Supervise(ctx context.Context, run func(context.Context)) error {
+	dryRun(ctx, run)
+	return nil
+}
+
+/*
+Idea...
 type NodeDefinition struct{}
 type NodeExecution struct{}
+*/
 
-type ImplicitGraph struct {
-	Definition []NodeDefinition
-	Execution  []NodeExecution
+func dryRun(ctx context.Context, run func(context.Context)) {
+	ctxValue := newDryRunContextValue()
+	ctx = context.WithValue(ctx, dryRunCtxKey, ctxValue)
+	run(ctx)
 }
 
-func dryRun(ctx context.Context, run func(context.Context) ImplicitGraph) {
-	ctx = context.WithValue(ctx, ctxKey, true)
-	implicitGraph := run(ctx)
+func getDryRun(ctx context.Context) (val *dryRunContextValue, ok bool) {
+	val, ok = ctx.Value(dryRunCtxKey).(*dryRunContextValue)
+	return
 }
 
-func IsDryRun(ctx context.Context) bool {
-	return ctx.Value(ctxKey).(bool)
+func RegisterDryRunNode(ctx context.Context) (isDryRun bool) {
+	val, ok := getDryRun(ctx)
+	if !ok {
+		return false
+	}
+	val.nodes = append(val.nodes, &dryRunNodes{
+		// TODO: add node definition
+	})
+	return true
+}
+
+func ErrNoContextValue[T any](key T) error { return fmt.Errorf("no value for key %v+", key) }
+
+func WithSideEffects(ctx context.Context, fun func()) {
+	if _, isDryRun := getDryRun(ctx); isDryRun {
+		return
+	}
+	fun()
 }
 
 func userCode() {
 	ctx := context.Background()
-	Supervise(ctx, func(ctx context.Context) ImplicitGraph {
-
-		return ImplicitGraph{
-			Definition: []NodeDefinition{},
-			Execution:  []NodeExecution{},
-		}
+	Supervise(ctx, func(ctx context.Context) {
 	})
 }
